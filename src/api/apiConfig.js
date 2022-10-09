c6c004b17f9560a1c7f9c59aa7c99defe96ceddc
PhyoTK <92428425+PhyoTK@users.noreply.github.com>
2022-10-09 23:13:10 +0630

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -2,8 +2,7 @@ const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: '36908693e7db052c9e5650c655406faa',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
-    videoUrl: 'https://blue-glitter-e23f.phyotk1602.workers.dev/1:/m_english/1917_2019/',
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
 
 export default apiConfig;
