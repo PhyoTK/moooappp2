682a4e0ff7084138b7419137ba515e7b1398193c
PhyoTK <92428425+PhyoTK@users.noreply.github.com>
2022-10-09 15:10:36 +0630

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '',
+    apiKey: '36908693e7db052c9e5650c655406faa',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
