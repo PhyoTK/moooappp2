6b1a0c4a0f64814b1db66e209c369ec30aa8c4ca
PhyoTK <92428425+PhyoTK@users.noreply.github.com>
2022-10-09 22:39:42 +0630

    Update VideoList.jsx

--- src/pages/detail/VideoList.jsx
+++ src/pages/detail/VideoList.jsx
@@ -46,7 +46,7 @@ const Video = props => {
                 <h2>{item.name}</h2>
             </div>
             <iframe
-                src={`https://youtu.be/embed/${item.key}`}
+                src={`https://www.youtube.com/embed/${item.key}`}
                 ref={iframeRef}
                 width="100%"
                 title="video"
