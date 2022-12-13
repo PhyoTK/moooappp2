6b08a98032a767d840cc38827cec4f1b0f415563
PhyoTK <92428425+PhyoTK@users.noreply.github.com>
2022-12-13 16:14:26 +0630

    Update Detail.jsx

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -11,7 +11,7 @@ import VideoList from './VideoList';
 import MovieList from '../../components/movie-list/MovieList';
 
 const tkmovie = () => {
-    console.log('potplayer://https://tkmovie.cf/6:/SERIES/Complete%20Series%20/2020/9-1-1%20(911)%202018/Season%203/Copy%20of%209-1-1%20(S3)%20-%20Episode%2017.mp4')
+    potplayer://https://tkmovie.cf/6:/SERIES/Complete%20Series%20/2020/9-1-1%20(911)%202018/Season%203/Copy%20of%209-1-1%20(S3)%20-%20Episode%2017.mp4)
 }
 
 const Detail = () => {
